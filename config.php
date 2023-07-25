447c0d775ff2ca537bcc176b34c53e802c28d9e3
xedashi <dmitrjj.klat97@gmail.com>
2023-07-25 18:17:59 +0600

    Создана директория проекта. Настройки в .htaccess, заданы константы в config.php и internal_settings.php. Объявлена точка входа index.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+defined(VG_ACCESS) or die("Access denied");
+const SITE_URL = 'http://site.learn';
+const PATH = '/';
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'db_site';
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+define('VG_ACCESS', true);
+header('Content-Type:text/html;charset=utf-8');
+session_start();
+
+require_once 'config.php';
+require_once 'core/base/setting/internal_settings.php';
