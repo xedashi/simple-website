71618614d434fe4cef8e19923d00529862599b00
xedashi <dmitrjj.klat97@gmail.com>
2023-07-26 23:18:43 +0600

    Реализован синглтон класс контроллера

--- core/base/controllers/RouteController.php
+++ core/base/controllers/RouteController.php
@@ -5,6 +5,9 @@ namespace core\base\controllers;
 class RouteController {
     static private $_instance;
     static public function getInstance() {
+        if(self::$_instance instanceof self) {
         return self::$_instance;
+        } 
+        return self::$_instance = new self;
     }
 }
