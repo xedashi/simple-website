25b37df8517e7d0a7b79757e2c8b7eb7820c5651
xedashi <dmitrjj.klat97@gmail.com>
2023-07-27 23:02:52 +0600

    Создан метод для расширения настроек отдельными плагинами.

--- core/base/settings/Settings.php
+++ core/base/settings/Settings.php
@@ -31,7 +31,10 @@ class Settings {
         ]
 
     ];
-
+    private $templateArr = [
+        'text' => ['name', 'phone', 'address'],
+        'textarea' => ['content', 'keywords']
+    ];
     private function __construct()
     {
         
@@ -39,11 +42,50 @@ class Settings {
     private function __clone() {
         
     }
+    static public function get($property){
+        return self::getInstance()->$property;
+    }
     public static function getInstance() {
         if(self::$_instance instanceof self) {
             return self::$_instance;
         }
         return self::$_instance = new self;
 
+    }
+    public function clueProperties($class) {
+        $baseProperties = [];
+
+        foreach($this as $name => $item) {
+            $property = $class::get($name);
+            if(is_array($property) && is_array($item)) {
+               $baseProperties[$name] = $this->arrayMergeRecursive($this->$name, $property); 
+               continue;
+            }   
+            if(!$property) $baseProperties[$name] = $this->$name; 
+            
+        }
+        return $baseProperties;
+    }
+    
+    public function arrayMergeRecursive() {
+        $arrays = func_get_args();
+        $base = array_shift($arrays);
+
+        foreach($arrays as $array) {
+            foreach($array as $key => $value) {
+                if(is_array($value) && is_array($base[$key])) {
+                    $base[$key] = $this->arrayMergeRecursive($base[$key], $value);
+                } else {
+                    if(is_int($key)) {
+                        if(!in_array($value, $base)) {
+                            array_push($base, $value);
+                        }
+                        continue;
+                    }
+                    $base[$key] = $value;
+                }
+            }
+        }
+        return $base;
     }
 }
