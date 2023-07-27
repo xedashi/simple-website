1582f6c71e52324ab43e7fde2ef95a5e955bf0c0
xedashi <dmitrjj.klat97@gmail.com>
2023-07-27 17:03:39 +0600

    Закрыты методы __clone и __construct

--- core/base/controllers/RouteController.php
+++ core/base/controllers/RouteController.php
@@ -4,6 +4,12 @@ namespace core\base\controllers;
 
 class RouteController {
     static private $_instance;
+    private function __construct() {
+
+    }
+    private function __clone() {
+        
+    }
     static public function getInstance() {
         if(self::$_instance instanceof self) {
         return self::$_instance;
